186d937b3f58c0df13784ef401e9ad193a3bd119
Ricardo Augusto Ferrari <ricardoaugusto.ferrari@gmail.com>
2017-08-19 21:01:31 -0300

    Criaçao do sistema mestre/detalhe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+export class Peca {
+	id: number;
+	nome: string;
+	categoria: string;
+	imagem: string;
+}
+
+const PECAS: Peca[] = [
+  { id: 11, nome: 'Calota', categoria: 'Acabamento', imagem: null },
+  { id: 12, nome: 'Adesivo', categoria: 'Acabamento', imagem: null },
+  { id: 13, nome: 'Vela', categoria: 'Elétrica', imagem: null }
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Autopeças';
+  selectedPeca: Peca;
+  pecas = PECAS;
+
+  onSelect(peca: Peca): void {
+	  this.selectedPeca = peca;
+  }
 }
